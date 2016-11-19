a762a0780bdc97b434854689145c39bbb76ae7a3
Jorge Peguero <JrgPeguero@yahoo.com>
2016-11-19 13:54:19 -0500

    add routes

---
+++ app/routing/html-routes.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+
+
+module.exports = function (app) {
+	// HTML GET Requests
+	// Below code handles when users "visit" a page.
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+
+	app.get('/washers', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/washers.html'));
+	});
+
+	app.get('/users', function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/users.html'));
+	});
+
+	// If no matching route is found default to home
+	app.use(function (req, res) {
+		res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+};
---
+++ server copy.js
@@ -0,0 +1,31 @@
+
+var bodyParser = require('body-parser');
+
+var express = require('express');
+
+var path = require('path');
+
+var app = express();
+ 
+
+ 
+var PORT = process.env.PORT || 8080;
+
+
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json 
+app.use(bodyParser.json());
+
+
+//require('./app/routing/api-routes.js')(app); 
+
+require ('./app/routing/api-routes.js')(app);
+require('./app/routing/html-routes.js')(app);
+
+ 
+app.listen(PORT,function(){
+	console.log("Laundr App is running on Port: "+PORT);
+});
