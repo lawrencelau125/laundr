5e49617669254349d7eebc58457b20579fd3e327
Jorge Peguero <JrgPeguero@yahoo.com>
2016-11-22 19:31:20 -0500

    server is working

--- app/public/js/init.js
+++ app/public/js/init.js
@@ -6,3 +6,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
+$( document ).ready(function() {
+    $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of do
+});
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
  
 var PORT = process.env.PORT || 8080;
 
-
+app.use(express.static('app/public'));
 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }));
